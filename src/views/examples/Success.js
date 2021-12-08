ac60b213daedafcf1091103e75a9cea850ba90fb
Moeen Mahmud <write2fahim007@gmail.com>
2021-12-08 14:13:59 +0600

    fixed minor text color issue

--- src/views/examples/Success.js
+++ src/views/examples/Success.js
@@ -4,8 +4,8 @@ import React from "react";
 const Success = () => {
   const { user } = useAuth();
   return (
-    <div className="" style={{ zIndex: 1000 }}>
-      <h1 className="display-3">
+    <div className="my-3" style={{ zIndex: 1000 }}>
+      <h1 className="display-3 text-light">
         {user.email && `Registration Successfull for ${user.displayName}`}
       </h1>
     </div>
